2795308d334002ec0114b5bd559150b29909d77a
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-06-21 23:01:16 +0700

    design sigup actvt UI and add scrollviewer to login actvt

---
+++ app/src/main/java/com/app/toko/viewmodels/SignupVewModel.java
@@ -0,0 +1,6 @@
+package com.app.toko.viewmodels;
+
+import androidx.databinding.BaseObservable;
+
+public class SignupVewModel extends BaseObservable {
+}
---
+++ app/src/main/java/com/app/toko/views/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.app.toko.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.toko.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
