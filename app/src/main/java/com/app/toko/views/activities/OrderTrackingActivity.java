16109b12d615c08b40379c5e1d506e262afe8da4
leehungw <hungttalop83@gmail.com>
2023-07-24 18:58:33 +0700

    leehungw - Add Order Tracking Activity (UI)

---
+++ app/src/main/java/com/app/toko/views/activities/OrderTrackingActivity.java
@@ -0,0 +1,16 @@
+package com.app.toko.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.toko.R;
+
+public class OrderTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_tracking);
+    }
+}
