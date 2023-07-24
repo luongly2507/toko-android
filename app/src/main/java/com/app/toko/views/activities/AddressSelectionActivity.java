eb441b38b6cb83649aecbafe76b403af5758dd0d
꧁༺ℰβ¡ꜱʊ༻꧂ <21521910@gm.uit.edu.vn>
2023-07-24 16:59:45 +0700

    add UI Address Selection 1

---
+++ app/src/main/java/com/app/toko/views/activities/AddressSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.app.toko.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddressSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
